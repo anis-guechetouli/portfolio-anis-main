aa4579b946b3379f162ec20b5b5aa780653fe0e4
Anis <anisguechetouli5@gmail.com>
2026-01-26 17:39:49 +0100

    Fix GitHub Pages basePath

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
+const repo = "portfolio-anis-main"
+
 const nextConfig = {
   output: "export",
   images: { unoptimized: true },
+  basePath: `/${repo}`,
+  assetPrefix: `/${repo}/`,
 }
 
 module.exports = nextConfig
